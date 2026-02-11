c1672f798d984cab2d1524f3767ad08223b7e7a3
Omkar Sontakke <omkarsontakke66527@gmail.com>
2026-02-11 16:50:17 +0530

    Add Custom exception for InsufficientBalanceException

---
+++ src/main/java/in/sp/main/customeexception/InsufficientBalanceException.java
@@ -0,0 +1,12 @@
+package in.sp.main.customeexception;
+
+public class InsufficientBalanceException extends RuntimeException{
+
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+
+    public InsufficientBalanceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
